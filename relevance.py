edf305df1c7fc4a25582cc098e72c023644aa834
khazana <haiyum95@gmail.com>
2019-04-19 13:53:22 -0400

    Update relevance.py
    makes sure that the numbers in CACM have four digits in them

--- relevance.py
+++ relevance.py
@@ -3,8 +3,11 @@ import pickle
 rel = {}
 relevance = open('cacm.rel.txt', "r").readlines()
 for item in relevance:
-    query_no = item.split('Q0')[0].strip()
+    query_no = int(item.split('Q0')[0].strip())
     document = item.split('Q0')[1].split(' 1')[0].strip()
+    number = document.split('-')[1]
+    number = "{0:04}".format(int(number))
+    document = 'CACM-' + number
     rel.setdefault(query_no,[]).append(document)
     
 with open('relevance.pickle','wb') as f:
